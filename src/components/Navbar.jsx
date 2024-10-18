438db351faa00357c5cfe0c7edac6c724175ef6c
Salman Ali <salmanafridi945@gmail.com>
2024-10-19 00:48:47 +0500

    workded on navabr component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../style";
+// import { NavLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { li, nav } from "framer-motion/client";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            {" "}
+            Salman <span className="sm:block hidden">| saloskilo</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => {
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>;
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
