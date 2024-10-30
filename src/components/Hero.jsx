095baf89e3980487c1e781dc78ddb641a3f75308
Salman Ali <salmanafridi945@gmail.com>
2024-10-31 00:20:50 +0500

    working on hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div
+          className="flex
+         flex-col justify-center items-center mt-5"
+        >
+          <div className="w-5 h-5 rounded-full bg-[#915eff]">
 
-export default Hero
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
